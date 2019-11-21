6b5c8e2e53faeb6218b77a796167b541702f8425
Rahul Yedida <yrahul3910@gmail.com>
2019-11-20 22:50:04 -0500

    Updated generate-tsv.py to match the required format

--- scripts/generate-tsv.py
+++ scripts/generate-tsv.py
@@ -3,7 +3,13 @@ import json
 with open('../extracted.json', 'r') as f:
     extracted = json.load(f)
 
-tsv = '\n'.join(list(map(lambda x: x['transcript'], extracted)))
+# Get passages
+passages = list(map(lambda x: x['transcript'], extracted))
+
+# Join words by marking them all class B. This is a test file, so we're only
+# really adding a random class because the format wants it. In reality, these
+# classes are what we want to predict ¯\_(ツ)_/¯
+words = '\n'.join(['\tB\n'.join(passage.split()) for passage in passages])
 
 with open('../biobert-data/extracted.tsv', 'w') as f:
-    f.write(tsv)
+    f.write(words)
