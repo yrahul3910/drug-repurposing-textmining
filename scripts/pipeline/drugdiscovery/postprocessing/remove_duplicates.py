37e90a863bf3d111da62ef45b27a9d84c73f96cc
Rahul Yedida <yrahul3910@gmail.com>
2019-12-04 17:49:09 -0500

    Possible fix to deduper

--- scripts/pipeline/drugdiscovery/postprocessing/remove_duplicates.py
+++ scripts/pipeline/drugdiscovery/postprocessing/remove_duplicates.py
@@ -10,11 +10,15 @@ class DuplicateRemover:
         Initializes a DuplicateRemover.
         :param terms: List of strings. Each string is a set of terms separated by spaces.
         """
-        self.terms_list = ' '.join(terms).split()
+        self.terms_list = terms
 
     def remove(self) -> list:
         """
         Removes adjacent duplicate terms. Code from https://stackoverflow.com/a/3463582.
         :return: List of terms, of which no adjacent terms are the same.
         """
-        return [key for key, group in itertools.groupby(self.terms_list)]
+        excerpts = []
+        for terms in self.terms_list:
+            excerpts.append([terms[i] for i in range(len(terms)) if i == len(terms) - 1 or terms[i] != terms[i + 1]])
+
+        return excerpts
